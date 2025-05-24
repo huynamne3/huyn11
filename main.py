27b31f6ac2332969f165782fbadf15afb175bde3
huynamne3 <huynamle024@gmail.com>
2025-05-24 23:59:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ def start_spam():
     return f"Đang chạy tool spam cho UID/link: {target_uid}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
